d6bf52e282983a85a705797aa4f92f100ad57b3d
David Eaves <null@crc1.net>
2023-12-31 17:04:33 -0500

    2023-12-31 17:04:33

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 # Default Startup Script if none found for the OS
+# It is generally recommended to use the OS specific startup script when supporting multiple OSes
+#
+# Example:
+# - scripts/startup_2076.sh    # Alpine Linux x64
+# - scripts/startup_2104.sh    # Ubuntu 23.04 x64
+# - scripts/startup_2136.sh    # Debian 12 x64 (bookworm)
+#
+# If a script is not found for the OS, this script will be used.
+#
